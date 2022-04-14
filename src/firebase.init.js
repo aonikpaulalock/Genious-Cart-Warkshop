be5313758c0ca5bf765682ac479e04df99e0d7cd
aonikpaulalocke <alockpaul9845@gmail.com>
2022-04-14 21:20:23 +0600

    projects done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -22,7 +22,7 @@ authDomain:process.env.REACT_APP_authDomain,
 projectId:process.env.REACT_APP_projectId,
 storageBucket:process.env.REACT_APP_storageBucket,
 messagingSenderId:process.env.REACT_APP_messagingSenderId,
-appId:process.env.REACT_APP_appId,
+appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
